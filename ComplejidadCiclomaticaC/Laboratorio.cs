27412cb63e363b07fa05bc07c7eb1d9fb7103727
RigelScript <enoch2705@hotmail.com>
2020-02-18 11:34:20 -0600

    modificaciones en ejercicio 1

---
+++ ComplejidadCiclomaticaC/Laboratorio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplejidadCiclomaticaC
+{
+     class Laboratorio
+    {
+
+        public  string  GetToxicidad(decimal grado)
+        {
+            if(grado<=3.8m)
+            return "VERDE";
+
+            if (grado <= 8.25m)
+                return "AZUL";
+            if (grado <= 15m)
+                return "AMARILLO";
+            return "ROJO";
+        }
+    }
+}
--- ComplejidadCiclomaticaC/UnitTest1.cs
+++ ComplejidadCiclomaticaC/UnitTest1.cs
@@ -7,8 +7,68 @@ namespace ComplejidadCiclomaticaC
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestToxicidad379()
         {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("VERDE", lab.GetToxicidad(3.79m));
+        }
+
+        [TestMethod]
+        public void TestToxicidad380()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("VERDE", lab.GetToxicidad(3.80m));
+        }
+        [TestMethod]
+        public void TestToxicidad381()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AZUL", lab.GetToxicidad(3.81m));
+        }
+        [TestMethod]
+        public void TestToxicidad824()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AZUL", lab.GetToxicidad(8.24m));
+        }
+
+        [TestMethod]
+        public void TestToxicidad825()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AZUL", lab.GetToxicidad(8.25m));
+        }
+        [TestMethod]
+        public void TestToxicidad826()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AMARILLO", lab.GetToxicidad(8.26m));
+        }
+
+        [TestMethod]
+        public void TestToxicidad827()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AMARILLO", lab.GetToxicidad(8.27m));
+        }
+
+        [TestMethod]
+        public void TestToxicidad1499()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AMARILLO", lab.GetToxicidad(14.99m));
+        }
+        [TestMethod]
+        public void TestToxicidad1500()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("AMARILLO", lab.GetToxicidad(15m));
+        }
+        [TestMethod]
+        public void TestToxicidad1501()
+        {
+            Laboratorio lab = new Laboratorio();
+            Assert.AreEqual("ROJO", lab.GetToxicidad(15.01m));
         }
     }
 }
